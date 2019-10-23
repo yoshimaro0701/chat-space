3f84e97def2fa4d4f080cc83f6b486fd13981a86
yoshimaro0701 <fc_bayern701@yahoo.co.jp>
2019-10-23 17:41:36 +0900

    グループにメッセージを表示&サイドバーに最新のメッセージを表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,6 @@
 class MessagesController < ApplicationController
   before_action :set_group
-  
+
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
@@ -14,6 +14,7 @@ class MessagesController < ApplicationController
       @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
+    end
   end
 
   private
@@ -24,4 +25,5 @@ class MessagesController < ApplicationController
 
   def set_group
     @group = Group.find(params[:group_id])
+  end
 end
