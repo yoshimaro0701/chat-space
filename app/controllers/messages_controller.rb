1333186d0b699c0e6ed7a6564093aa421049211a
yoshimaro0701 <fc_bayern701@yahoo.co.jp>
2019-10-27 16:51:07 +0900

    jbuilerで作成したメッセージをJSON形式で返す

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,6 @@ class MessagesController < ApplicationController
   end
 
   def create
-    binding.pry
     @message = @group.messages.new(message_params)
     if @message.save
       redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
@@ -15,6 +14,11 @@ class MessagesController < ApplicationController
       @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
+    end
+
+    respond_to do |format|
+      format.html
+      format.json
     end
   end
 
