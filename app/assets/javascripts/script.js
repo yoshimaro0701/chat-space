b4dbbceb144e37b3d838d8f848cd0e1d436f150d
yoshimaro0701 <fc_bayern701@yahoo.co.jp>
2019-10-27 16:15:13 +0900

    イベント発火&messages#createが動くようにする

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,8 @@
+//初回読み込み、リロード、ページ切り替えで動く。
+$(document).on('turbolinks:load', function() { });
+
+//初回読み込み、ページ切り替えで動く。リロードは動かない
+$(document).on('turbolinks:render', function() {　});
+
+//ページ遷移前に行いたい処理用。ページ切り替えでもリロードでも動かない
+$(document).on('turbolinks:request-start', function() {　});
